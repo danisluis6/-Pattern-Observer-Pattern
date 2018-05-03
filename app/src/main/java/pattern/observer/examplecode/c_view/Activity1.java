048e48cdf67d951e171e33841b7df5d961151b3e
lorence <lorence@enclave.vn>
2018-05-03 11:42:26 +0700

    Adjust infor

--- app/src/main/java/pattern/observer/examplecode/c_view/Activity1.java
+++ app/src/main/java/pattern/observer/examplecode/c_view/Activity1.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import pattern.observer.examplecode.R;
 
-public class Activity1 extends AppCompatActivity {
+public class Activity1 extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
