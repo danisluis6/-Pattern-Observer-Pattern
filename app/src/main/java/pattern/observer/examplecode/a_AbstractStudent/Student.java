6ef6b22b5380462eddc5e07e6f1593bd94104d7a
lorence <lorence@enclave.vn>
2018-05-03 14:22:57 +0700

    Continue with Observer design pattern

--- app/src/main/java/pattern/observer/examplecode/a_AbstractStudent/Student.java
+++ app/src/main/java/pattern/observer/examplecode/a_AbstractStudent/Student.java
@@ -3,7 +3,7 @@ package pattern.observer.examplecode.a_AbstractStudent;
 import pattern.observer.examplecode.b_complementary_method.Observer;
 
 public interface Student {
-    public void register(Observer observer);
-    public void unregister(Observer observer);
-    public void notifyObservers();
+    void register(Observer observer);
+    void unregister(Observer observer);
+    void notifyObservers();
 }
--- app/src/main/java/pattern/observer/examplecode/b_complementary_method/Observer.java
+++ app/src/main/java/pattern/observer/examplecode/b_complementary_method/Observer.java
@@ -1,5 +1,5 @@
 package pattern.observer.examplecode.b_complementary_method;
 
 public interface Observer {
-    public void update(final boolean checked);
+    void update(final boolean checked);
 }
